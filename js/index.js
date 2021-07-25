c242adccfe17015c84f3ed183f69116cb10dd3f7
Ivan D. Velez <ivancho175@hotmail.com>
2021-07-25 16:52:37 -0500

    Tercer commit del proyecto. Agrego media querys y la hamburguesa. Falta ícono de búsqueda en el navBar superior.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navItems = document.querySelectorAll('.nav__item');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
