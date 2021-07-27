ac8e59456359e05425a6db85e0681ed87a483741
Ivan D. Velez <ivancho175@hotmail.com>
2021-07-27 16:31:41 -0500

    Termino el desarrollo del proyecto. La barra de busqueda ubica la categoria que se le ingrese (solo en modo desk) y nos desplaza hasta la grilla. las imagenes tienen una descripcion en el hover (en modo Masonry) o en la descripcion de la imagen (en modo List). los iconos de cambio de display tienen funcionalidad completa. Fui muy feliz desarrollando este proyecto. Gracias a Tita media por el diseño y la oportunidad ;)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//CREAMOS VARIABLES PARA NUESTRO MENU RESPOMSIVO
+//CREAMOS VARIABLES PARA NUESTRO MENU RESPONSIVO
 const navToggle = document.querySelector('.nav-toggle');
 const navItems = document.querySelectorAll('.nav__item');
 
@@ -12,12 +12,18 @@ navItems.forEach((item) => {
   });
 }); //API KEY DE UNSPLASH
 const API_KEY = 'A6iCpX-5swdyUEqIhjKanx4538a7GWpwZvXT65JNSEk';
-// LLAMAMOS EL ELEMENTO CONTENEDOR
+// LLAMAMOS LOS ELEMENTOS CONTENEDORES
+let portfolio = document.querySelector('#grid');
 let container = document.querySelector('.container');
 // LLAMAMOS LOS BOTONES DEL NAV
 let buttons = document.querySelectorAll('.nav__item');
 //LLAMAMOS EL BOTON QUE CARGA MAS IMAGENES
-let loadMoreBtn = document.getElementById('more');
+let loadMoreBtn = document.querySelector('#more');
+//LLAMAMOS LOS ICONOS DE CAMBIO DE DISPLAY
+let gallery = document.querySelector('#gallery');
+let list = document.querySelector('#list');
+//LLAMAMOS EL FORM DE LA BARRA DE BÚSQUEDA
+let form = document.querySelector('#searchForm');
 
 //VARIABLE DE LA CATEGORIA ACTUAL
 let actualCategory = 'all';
@@ -31,20 +37,39 @@ const FETCHAPI = async (url) => {
   return data;
 };
 
-//FUNCION QUE CREA LOS ELMENTOS IMAGE
-const createImgElement = (imagePath, altText) => {
-  let DOMImage = document.createElement('img');
-  DOMImage.src = `${imagePath}`;
-  DOMImage.classList.add('image');
-  DOMImage.alt = altText;
-  return DOMImage;
+//FUNCION QUE CREA LOS ELEMENTOS IMAGE
+const createImgElement = (imagePath, altText, title) => {
+  let itemContainer = document.createElement('div');
+  itemContainer.classList.add('item-container');
+  let dataContainer = document.createElement('div');
+  dataContainer.classList.add('item-container__data');
+  let image = document.createElement('img');
+  image.classList.add('item-container__image');
+  let dataTitle = document.createElement('h1');
+  dataTitle.classList.add('item-container__data-title');
+  let dataText = document.createElement('p');
+  dataText.classList.add('item-container__data-text');
+  image.src = `${imagePath}`;
+  image.alt = `${altText ? altText : 'No description available'}`;
+  dataTitle.innerText = `${title ? title : 'No title available'}`;
+  dataText.innerText = `${altText ? altText : 'No description available'}`;
+  dataContainer.appendChild(dataTitle);
+  dataContainer.appendChild(dataText);
+  itemContainer.appendChild(image);
+  itemContainer.appendChild(dataContainer);
+  return itemContainer;
 };
 
 //FUNCION QUE PINTA LAS IMAGENES EN EL DOM
 const printImages = (category) => {
   let allImages = category.map((item) => {
-    return createImgElement(item.urls.regular, item.alt_description);
+    return createImgElement(
+      item.urls.regular,
+      item.alt_description,
+      item.description
+    );
   });
+
   if (page == 1) {
     container.innerHTML = null;
   }
@@ -69,6 +94,7 @@ const loadData = async (category) => {
 
 //FUNCION QUE HACE EL LOAD INICIAL AL CARGAR LA PAGINA
 const firstLoad = async () => {
+  portfolio.classList.add('masonry');
   let results = await loadData(actualCategory);
   printImages(results);
 };
@@ -80,7 +106,39 @@ const loadMore = async () => {
   printImages(results);
 };
 
+//FUNCION PARA ENCONTRAR LA POSICIÓN DE LA GRILLA EN EL DOM
+const goToGrid = portfolio.getBoundingClientRect();
+
+//FUNCION PARA TRAER LAS IMÁGENES USANDO LA BARRA DE BÚSQUEDA
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  actualCategory = e.srcElement[0].value;
+  window.scroll(goToGrid);
+  e.srcElement[0].value = '';
+  let results = await loadData(actualCategory);
+  printImages(results);
+};
+
+//FUNCION QUE REMUEVE CLASE 'LISTA' Y AGREGA LA CLASE 'MASONRY' AL PORTFOLIO
+const setMasonryLayout = () => {
+  portfolio.classList.remove('list');
+  portfolio.classList.add('masonry');
+};
+
+const setListLayout = () => {
+  portfolio.classList.remove('masonry');
+  portfolio.classList.add('list');
+};
+
 //LISTENER DE CADA BOTON DEL NAVBAR
 buttons.forEach((button) => button.addEventListener('click', handleClick));
+//LISTENER DEL LOAD INICIAL
 document.addEventListener('DOMContentLoaded', firstLoad);
+//LISTENER DEL BOTÓN CARGAR MÁS
 loadMoreBtn.addEventListener('click', loadMore);
+//LISTENER DEL FORMULARIO DE BÚSQUEDA
+form.addEventListener('submit', handleSubmit);
+//LISTENER DEL LAYOUT MASONRY
+gallery.addEventListener('click', setMasonryLayout);
+//LISTENER DEL LAYOUT LIST
+list.addEventListener('click', setListLayout);
